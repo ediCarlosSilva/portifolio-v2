82a7513cb42f4030f06c543de2a7d27ebc26584a
Edi Carlos <edi_csilva@hotmail.com>
2023-04-26 14:54:20 -0300

    Section sobremim

---
+++ src/components/SobreMim/index.jsx
@@ -0,0 +1,14 @@
+import './SobreMim.css';
+
+export default function SobreMim() {
+  return (
+    <section className="about" id="sobremim">
+      <div className="about__container">
+        <h3 className="about__title">About me</h3>
+        <p className="about__paragraph">Talking about ourselves can be a challenging task, but by analyzing myself, I realize that I am a person who likes to learn about different areas, although some are more detailed and frequent than others. It is very important for me to use this acquired knowledge to help not only those who are close to me, but also the brothers who cross my path in this Earth. I have a beautiful family, with a wife and four children, one of whom is already in heaven, and everything I do it is in search of blessing them.</p>
+        <p className="about__paragraph">I currently work as an IT manager in a supermarket chain and I am a systems analyst. To improve myself in the area of IT infrastructure and development, especially in front-end, I have been studying a lot. In addition, I have a passion for learning languages and have already tried to study several of them.</p>
+        <p className="about__paragraph">I'm glad you're here and I wish you the choicest blessings of the Lord God, Almighty.</p>
+      </div>
+    </section>
+  )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
+import SobreMim from "../../components/SobreMim"
 import Title from "../../components/Title"
 
 export default function Home() {
     return (
-        <Title />
+        <>
+            <Title />
+            <SobreMim />
+        </>
     )
 }
