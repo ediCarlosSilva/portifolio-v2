450531d45d9bf9e5e1d8c88de06a0b51d552e049
Edi Carlos <edi_csilva@hotmail.com>
2023-04-29 08:39:42 -0300

    ajuste formulario

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,6 +1,40 @@
+import { useState } from 'react';
 import './Formulario.css';
 
 export default function Formulario() {
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [assunto, setAssunto] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  const atualizaNome = (evento) => {
+    let tempNome = evento.target.value;
+
+    if (tempNome > 50) {
+      
+    }
+
+    setNome(tempNome);
+  }
+
+  function atualizaEmail(evento) {
+    setEmail(evento.target.value)
+  }
+
+  function atualizaAssunto(evento) {
+    setAssunto(evento.target.value)
+  }
+
+  function atualizaMensagem(evento) {
+    setMensagem(evento.target.value);
+  }
+
+  function enviarForm(evento) {
+    evento.preventDefault();
+    console.log(nome, email, assunto, mensagem);
+  }
+
   return (
     <section className="formcontato">
       <div id="contato" className="formcontato__contacto">
@@ -18,28 +52,28 @@ export default function Formulario() {
           </ul>
         </div>
         <div className="formcontato__text">
-          <form className="formcontato__form" id="my-form" action="https://formspree.io/f/xrgvozjb" method="POST">
+          <form onSubmit={enviarForm} className="formcontato__form" id="my-form" action="https://formspree.io/f/xrgvozjb" method="POST">
             <div className="input-container">
-              <input name="nome" id="nome" className="formcontato__input" type="text" placeholder="Nome" required maxlength="51" />
-              <label for="nome" className="input-label" id="inputLabelNome">Name</label>
+              <input value={nome} onChange={atualizaNome} name="nome" id="nome" className="formcontato__input" type="text" placeholder="Nome" required maxLength="51" />
+              <label htmlFor="nome" className="input-label" id="inputLabelNome">Name</label>
               <span className="mensagem-erro-nome">Please, only 50 letters.</span>
             </div>
 
             <div className="input-container">
-              <input name="email" id="email" className="formcontato__input" type="email" placeholder="Email" required pattern="^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$" />
-              <label for="email" className="input-label" id="input__label--email">Email</label>
+              <input value={email} onChange={atualizaEmail} name="email" id="email" className="formcontato__input" type="email" placeholder="Email" required pattern="^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$" />
+              <label htmlFor="email" className="input-label" id="input__label--email">Email</label>
               <span className="mensagem-erro-email">Invalid E-mail format.</span>
             </div>
 
             <div className="input-container">
-              <input name="assunto" id="assunto" className="formcontato__input" type="text" placeholder="Assunto" required maxlength="51" />
-              <label for="assunto" className="input-label" id="inputLabelAssunto">Subject</label>
+              <input value={assunto} onChange={atualizaAssunto} name="assunto" id="assunto" className="formcontato__input" type="text" placeholder="Assunto" required maxLength="51" />
+              <label htmlFor="assunto" className="input-label" id="inputLabelAssunto">Subject</label>
               <span className="mensagem-erro-assunto">Please, only 50 letters.</span>
             </div>
 
             <div className="input-container">
-              <textarea name="mensagem" id="mensagem" className="formcontato__textarea" rows="5" cols="40" placeholder="Sua mensagem" required maxlength="301"></textarea>
-              <label for="mensagem" className="input-label" id="labelTextareaMensagem">Message</label>
+              <textarea value={mensagem} onChange={atualizaMensagem} name="mensagem" id="mensagem" className="formcontato__textarea" rows="5" cols="40" placeholder="Sua mensagem" required maxLength="301" ></textarea>
+              <label htmlFor="mensagem" className="input-label" id="labelTextareaMensagem">Message</label>
               <span className="mensagem-erro-textarea">Please, only 300 letters.</span>
             </div>
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,6 +9,9 @@ import SobreMim from "../../components/SobreMim"
 import Title from "../../components/Title"
 
 export default function Home() {
+
+    
+
     return (
         <>
             <Title />
