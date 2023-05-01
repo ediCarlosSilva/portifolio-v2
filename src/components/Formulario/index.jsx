7f2ecaa4a4e39df3be1d15b327bf0c363029c8dd
Edi Carlos <edi_csilva@hotmail.com>
2023-05-01 14:08:03 -0300

    portifiolio

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -11,10 +11,10 @@ export default function Formulario() {
   const [email, setEmail] = useState('');
   const [assunto, setAssunto] = useState('');
   const [mensagem, setMensagem] = useState('');
-  const [erroNome, setErroNome] = useState({nome: {valido: true, texto: ""}});
-  const [erroEmail, setErroEmail] = useState({email: {valido: true, texto: ""}});
-  const [erroAssunto, setErroAssunto] = useState({assunto: {valido: true, texto: ""}});
-  const [erroMensagem, setErroMensagem] = useState({mensagem: {valido: true, texto: ""}});
+  const [erroNome, setErroNome] = useState({ nome: { valido: true, texto: "" } });
+  const [erroEmail, setErroEmail] = useState({ email: { valido: true, texto: "" } });
+  const [erroAssunto, setErroAssunto] = useState({ assunto: { valido: true, texto: "" } });
+  const [erroMensagem, setErroMensagem] = useState({ mensagem: { valido: true, texto: "" } });
 
   const [state, handleSubmit] = useForm("xrgvozjb");
 
@@ -22,10 +22,10 @@ export default function Formulario() {
     let tempNome = evento.target.value;
 
     if (tempNome.length > 50) {
-      setErroNome({ nome: {valido: false, texto: "Sorry, only 50 letters."}})
+      setErroNome({ nome: { valido: false, texto: "Sorry, only 50 letters." } })
     } else {
       setNome(tempNome);
-      setErroNome({nome: {valido: true, texto: ""}})
+      setErroNome({ nome: { valido: true, texto: "" } })
     }
   }
 
@@ -37,11 +37,11 @@ export default function Formulario() {
     let tempEmail = evento.target.value;
 
     if (validator.isEmail(tempEmail)) {
-      setErroEmail({email: {valido: true, texto: ""}})
+      setErroEmail({ email: { valido: true, texto: "" } })
     } else if (validator.isEmpty(tempEmail)) {
-      setErroEmail({email: {valido: true, texto: ""}})
+      setErroEmail({ email: { valido: true, texto: "" } })
     } else {
-      setErroEmail({email: {valido: false, texto: "Enter a valid e-mail, please"}})
+      setErroEmail({ email: { valido: false, texto: "Enter a valid e-mail, please" } })
     }
 
   }
@@ -50,10 +50,10 @@ export default function Formulario() {
     let tempAssunto = evento.target.value;
 
     if (tempAssunto.length > 50) {
-      setErroAssunto({ assunto: {valido: false, texto: "Sorry, only 50 letters."}})
+      setErroAssunto({ assunto: { valido: false, texto: "Sorry, only 50 letters." } })
     } else {
       setAssunto(tempAssunto);
-      setErroAssunto({assunto: {valido: true, texto: ""}})
+      setErroAssunto({ assunto: { valido: true, texto: "" } })
     }
   }
 
@@ -61,21 +61,20 @@ export default function Formulario() {
     let tempMensagem = evento.target.value;
 
     if (tempMensagem.length > 300) {
-      setErroMensagem({mensagem: {valido: false, texto: "Sorry, only 300 letters."}})
+      setErroMensagem({ mensagem: { valido: false, texto: "Sorry, only 300 letters." } })
     } else {
       setMensagem(tempMensagem);
-      setErroMensagem({mensagem: {valido: true, texto: ""}})
+      setErroMensagem({ mensagem: { valido: true, texto: "" } })
     }
   }
 
-  // function enviarForm(evento) {
-  //   evento.preventDefault();
-  //   if (state.succeeded) {
-  //     return <p>Thanks for joining!</p>;
-  // }
-  //   if ()
-  //   console.log(nome, email, assunto, mensagem);
-  // }
+  if (state.succeeded) {
+    // console.log("Form enviado com sucesso.")
+    // setNome('');
+    // setEmail('');
+    // setAssunto('');
+    // setMensagem('');
+  }
 
   return (
     <Container component="section" maxWidth="lg" sx={{ mb: 3, mt: 3 }}>
@@ -103,20 +102,20 @@ export default function Formulario() {
 
           <form onSubmit={handleSubmit} id="my-form">
 
-            <TextField 
-              value={nome} 
-              onChange={atualizaNome} 
-              name="nome" 
-              variant='outlined' 
-              error={!erroNome.nome.valido} 
-              helperText={erroNome.nome.texto} 
-              fullWidth 
-              label="Nome" 
-              margin="normal" 
-              sx={{input: {background: "white"}}} 
+            <TextField
+              value={nome}
+              onChange={atualizaNome}
+              name="nome"
+              variant='outlined'
+              error={!erroNome.nome.valido}
+              helperText={erroNome.nome.texto}
+              fullWidth
+              label="Nome"
+              margin="normal"
+              sx={{ input: { background: "white" } }}
             />
 
-            <TextField 
+            <TextField
               variant='outlined'
               value={email}
               name="E-mail"
@@ -124,45 +123,45 @@ export default function Formulario() {
               onBlur={validaEmail}
               error={!erroEmail.email.valido}
               helperText={erroEmail.email.texto}
-              fullWidth 
-              label="Email" 
-              margin="normal" 
-              sx={{input: {background: "white"}}} 
+              fullWidth
+              label="Email"
+              margin="normal"
+              sx={{ input: { background: "white" } }}
             />
 
-            <TextField 
+            <TextField
               value={assunto}
               onChange={atualizaAssunto}
               error={!erroAssunto.assunto.valido}
               helperText={erroAssunto.assunto.texto}
               name="Assunto"
-              variant='outlined' 
-              fullWidth 
-              label="Assunto" 
-              margin="normal" 
-              sx={{input: {background: "white"}}} 
+              variant='outlined'
+              fullWidth
+              label="Assunto"
+              margin="normal"
+              sx={{ input: { background: "white" } }}
             />
 
-            <TextField 
+            <TextField
               value={mensagem}
               onChange={atualizaMensagem}
               error={!erroMensagem.mensagem.valido}
               helperText={erroMensagem.mensagem.texto}
               name="mensagem"
-              variant='outlined' 
-              fullWidth 
-              multiline 
-              rows={4} 
-              label="Mensagem" 
-              inputProps={{ maxLength: 301 }} 
+              variant='outlined'
+              fullWidth
+              multiline
+              rows={4}
+              label="Mensagem"
+              inputProps={{ maxLength: 301 }}
               margin="normal"
-              sx={{input: {background: "white"}, textarea: {background: "white"} }} 
+              sx={{ input: { background: "white" }, textarea: { background: "white" } }}
             />
 
             <Typography align="center" mt={2}>
-              <Button 
-                type="submit" 
-                variant="contained" 
+              <Button
+                type="submit"
+                variant="contained"
                 disabled={state.submitting}>Send Message</Button>
             </Typography>
 
